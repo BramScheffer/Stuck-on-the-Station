3666861d94bdba1ddeabfa301ae22218235e6793
potatoFinn <159907040+potatoFinn@users.noreply.github.com>
2024-09-09 14:55:35 +0200

    movement/cameraswitch

---
+++ My project/Assets/Scripts/CameraSwitch.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwitch : MonoBehaviour
+{
+    public Camera playCam;
+    public Camera buyCam;
+    private Camera activeCamera;
+    public bool camswitched;
+    // Start is called before the first frame update
+    void Start()
+    {
+        activeCamera = playCam;
+        playCam.enabled = true;
+        buyCam.enabled = false;
+        camswitched = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.G)) // Detects a single press of the "G" key
+        {
+            if (!camswitched)
+            {
+                camswitched = true;
+                SwitchCamera();
+            }
+        }
+    }
+
+
+    public void SwitchCamera()
+    {
+       
+        // Switch active camera
+        if (activeCamera == playCam)
+        {
+            playCam.enabled = false;
+            buyCam.enabled = true;
+            activeCamera = buyCam;
+            camswitched = false;
+        }
+        else
+        {
+            playCam.enabled = true;
+            buyCam.enabled = false;
+            activeCamera = playCam;
+            camswitched = false;
+        }
+    }
+}
