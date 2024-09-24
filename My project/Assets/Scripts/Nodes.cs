163b1babf432ca90e77544f9994eef909019c422
potatoFinn <159907040+potatoFinn@users.noreply.github.com>
2024-09-24 14:24:49 +0200

    energyScript

--- My project/Assets/Scripts/Nodes.cs
+++ My project/Assets/Scripts/Nodes.cs
@@ -10,43 +10,48 @@ public class Nodes : MonoBehaviour
     private GameObject turret;
     public Vector3 positionOffset;
     public CameraSwitch camswitch;
-    
+
+    // Reference to the EnergyManager
+    private EnergyManager energyManager;
+
     // Start is called before the first frame update
     void Start()
     {
         rend = GetComponent<Renderer>();
         startColor = rend.material.color;
-        
+
+        // Use FindObjectOfType if FindAnyObjectByType is not available
+        energyManager = FindObjectOfType<EnergyManager>();
+
+        if (energyManager == null)
+        {
+            Debug.LogError("EnergyManager not found in the scene!");
+        }
     }
 
     private void OnMouseDown()
     {
         if (turret != null)
         {
-            Debug.Log("Can't built there!");
+            Debug.Log("Can't build there!");
             return;
         }
 
-
-        if (camswitch.inBuy == true)
+        if (camswitch.inBuy)
         {
             GameObject turretToBuild = BuildMenager.Instance.GetTurretToBuild();
             turret = Instantiate(turretToBuild, transform.position + positionOffset, transform.rotation);
+            energyManager.energyIncreaseAmount += 0.15f; // Increase energy amount
         }
-        
     }
-
-
 
     private void OnMouseEnter()
     {
         rend.material.color = hoverColor;
     }
 
-
     private void OnMouseExit()
     {
         rend.material.color = startColor;
     }
-
 }
--- My project/Assets/TowerScripts/Energy tower/EnergyManager.cs
+++ My project/Assets/TowerScripts/Energy tower/EnergyManager.cs
@@ -1,18 +1,46 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Timers;
 using UnityEngine;
 
 public class EnergyManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
+    // The amount of energy to increase every second
+    [Range(0f, 10f)] // This allows you to set a value between 0 and 10 in the Inspector
+    public float energyIncreaseAmount = 1.0f;
+
+    // Current energy level
+    private float energy = 0.0f;
+
+    // Timer to track time
+    private float timer = 0.0f;
+
+    public GameObject train;
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Increase timer by the time since the last frame
+        timer += Time.deltaTime;
+
+        // Check if one second has passed
+        if (timer >= 1.0f)
+        {
+            IncreaseEnergy();
+            timer = 0.0f; // Reset timer
+        }
+        if (energy >= 100f)
+        {
+            train.transform.Translate(Vector3.forward * 5.0f * Time.deltaTime);
+        }
+    }
+
+    // Method to increase energy
+    private void IncreaseEnergy()
+    {
+        energy += energyIncreaseAmount;
+        Debug.Log("Current Energy: " + energy);
     }
 }
