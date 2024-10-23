00d975602cd5edd7385b3fa88fcde2035598a413
BramScheffer <140196370+BramScheffer@users.noreply.github.com>
2024-10-23 16:27:35 +0200

    Main menu cursor

--- My project/Assets/Bram Assets/Animations/mainMenu.cs
+++ My project/Assets/Bram Assets/Animations/mainMenu.cs
@@ -1,16 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Net.NetworkInformation;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class mainMenu : MonoBehaviour
 {
-    
     public Canvas main;
     public Canvas settings;
-  public void PlayClicked()
+
+    // This is called when the script is loaded
+    void Start()
     {
+        // Make the cursor visible and unlocked when in the main menu
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    public void PlayClicked()
+    {
+        // Optional: Hide the cursor and lock it when starting the game
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+
         SceneManager.LoadScene("Level1");
     }
 
@@ -18,13 +29,11 @@ public class mainMenu : MonoBehaviour
     {
         main.enabled = false;
         settings.enabled = true;
-
-        
     }
 
     public void OnApplicationQuit()
     {
-     Application.Quit();
+        Application.Quit();
         print("Game Quitted");
     }
 }
